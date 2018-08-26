f264cbe3131af31d2147bcc020feb9e351cd7b85
Anna Kowalczuk <anna@kowalczuk.eu>
2018-08-26 14:47:16 +0200

    Disable button after click

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
     var prefix = "https://cors-anywhere.herokuapp.com/";
     var tweetLink = "https://twitter.com/intent/tweet?text=";
     var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+    var button = $('.trigger');
 
     function getQuote() {
         $.getJSON(prefix + quoteUrl, createTweet);
@@ -10,15 +11,6 @@
 
     $.ajaxSetup({ cache: false });
     
-    // function getQuote() {
-    //     $.ajax({
-    //         dataType: "json",
-    //         url: quoteUrl,
-    //         data: null,
-    //         success: createTweet()
-    //     });
-    // };
-
     function createTweet(input) {
         var data = input[0];
 
@@ -37,12 +29,14 @@
             $('.quote').text(quoteText);
             $('.author').text("Author: " + quoteAuthor);
             $('.tweet').attr('href', tweet);
+            button.prop('disabled', false);
         }
     };
 
     $(document).ready(function() {
         getQuote();
         $('.trigger').click(function() {
+            button.prop('disabled', true);
             getQuote();
         })
     });
